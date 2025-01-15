7ea8a685e01cbad4255300cbf478faaf7f6dfdc1
megermer <113950667+megermer@users.noreply.github.com>
2025-01-15 10:42:20 -0500

    Adjust weights for SM2 algo

--- bert/bert.py
+++ bert/bert.py
@@ -9,5 +9,11 @@ def bert_model(card_back, user_input):
 
     similarity = util.cos_sim(card_back_embedding, user_input_embedding)
 
+    print(similarity.item())
     return similarity.item()
+
+s1 = "I love pizza"
+s2 = "I really like pizza"
+
+bert_model(s1, s2)
 
--- jsversion/backend/SM2.jsx
+++ jsversion/backend/SM2.jsx
@@ -1,6 +1,5 @@
 /**
  * @typedef {Object} SuperMemoItem
- * @property {number} interval - The current interval for the item.
  * @property {number} repetition - The current repetition count.
  * @property {number} efactor - The current ease factor.
  * @property {string} question - The question.
@@ -20,36 +19,24 @@
  * @returns {SuperMemoItem} The updated item with new interval, repetition, and efactor.
  */
 export default function supermemo(item, grade) {
-    let nextInterval;
     let nextRepetition;
     let nextEfactor;
   
     console.log('item.efactor: ', item.efactor)
     console.log('grade', grade)
     if (grade >= 2) {
-      if (item.repetition === 0) {
-        nextInterval = 1;   // Whenever we see the card next
-        nextRepetition = 1; // Incrementing the number of correct responses
-      } else if (item.repetition === 1) {
-        nextInterval = 6;
-        nextRepetition = 2;
-      } else {
-        nextInterval = Math.round(item.interval * item.efactor);
-        nextRepetition = item.repetition + 1;
-      }
+      nextRepetition = item.repetition + 1;
     } else {
-      nextInterval = 1;
       nextRepetition = 0;
     }
   
-    nextEfactor = item.efactor + (0.1 - (5 - (grade)*(1.66)) * (0.08 + (5 - (grade)*(1.66)) * 0.02));  
+    nextEfactor = item.efactor + (0.1 - (3 - grade) * (0.08 + (3 - grade) * 0.02));  
 
-    if (nextEfactor < 1.3) nextEfactor = 1.3;
+    if (nextEfactor < 2.3) nextEfactor = 2.3;
   
     console.log("new efactor", nextEfactor)
 
     return {
-      interval: nextInterval,
       repetition: nextRepetition,
       efactor: nextEfactor,
       question: item.question,
